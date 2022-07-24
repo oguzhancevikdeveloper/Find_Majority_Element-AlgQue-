d3bcfa4ca11f4336dd0f52a7f3137a4c1a239da1
oguzhan.cevik <oguzhancevik.developer@gmail.com>
2022-07-24 14:18:01 +0300

    Add project files.

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MajorityElement
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] myList = {1,1,1,2,2,3,4,4,1,1,3,3,3,3,3,3,3};
+      int result = 0;
+      int count = 0;
+
+      foreach (var item in myList)
+      {
+        if(count == 0)
+        {
+          result = item;
+          if (item == result)
+          {
+            count++;
+          }
+        }
+        else
+        {
+          count--;
+        }
+      }
+      Console.WriteLine("En çok tekrar eden sayı: " + result);
+      Console.ReadLine();
+    }
+  }
+}
